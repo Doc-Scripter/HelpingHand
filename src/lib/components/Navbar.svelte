3dcb6151326769884816a11904b93189d93d424e
cliffootieno <clifford242526@gmail.com>
2025-06-10 17:59:33 +0300

    feat(layout): add navbar component and global styles
    Implement basic layout structure with navbar component and global CSS styles

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+  // You can add reactive variables or props here
+</script>
+
+<nav class="navbar">
+  <div class="navbar-brand">
+    <a href="/">Helping Hand</a>
+  </div>
+  <ul class="navbar-links">
+    <li><a href="/">Home</a></li>
+    <li><a href="/projects">Projects</a></li>
+    <li><a href="/donate">Donate</a></li>
+    <li><a href="/admin">Admin</a></li>
+  </ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+  import '/static/css/global.css'; // Import your global CSS if you have one
+</script>
+
+<Navbar />
+
+<slot />
