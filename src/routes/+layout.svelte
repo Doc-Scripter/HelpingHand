e1302aad9fd1e9d88a03b22da4eb3d460a3678be
cliffootieno <clifford242526@gmail.com>
2025-06-10 18:22:36 +0300

    feat(layout): add Footer component to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
   import Navbar from '$lib/components/Navbar.svelte';
+  import Footer from '$lib/components/Footer.svelte';
   import '/static/css/global.css'; // Import your global CSS if you have one
 </script>
 
 <Navbar />
 
 <slot />
+
+<Footer />
